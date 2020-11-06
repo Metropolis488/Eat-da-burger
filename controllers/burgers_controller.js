8a3afe1c4161c102164bd43d7e63d95c4c802be5
Matt Steiner <Metropolis.4488@gmail.com>
2020-11-05 21:16:17 -0500

    added controller

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -4,5 +4,39 @@ var router = express.Router();
 var burger = require("../models/burger.js");
 
 router.get("/", function(req, res) {
-    
+    burger.all(function(data) {
+        var hbsObject = {
+            burgers: data
+        };
+        console.log(hbsObject);
+        res.render("index", hbsObject);
+    });
+});
+
+router.post("/api/burgers", function(req, res) {
+    burger.create([
+        "burgerName", "devoured"
+    ], [
+        req.body.burgerName, req.body.devoured
+    ], function(result) {
+        res.json({ id: result.insertId });
+    });
+});
+
+router.put("/api/burgers/:id", function(req, res) {
+    var condition = "id = " + req.params.id;
+
+    console.log("condition", condition);
+
+    burger.update({
+        devoured: req.body.devoured
+    }, condition, function(result) {
+        if (result.changeRows == 0) {
+        return res.status(404).end();
+    } else {
+        res.status(200).end();
+    }
+});
 })
+
+module.exports = router;
